f01afad53abbbea05dfd25b1bb4e4b0666198b96
Emmanuel Asante <emmanuela.asante@gmail.com>
2016-07-19 01:03:31 -0600

    Grant editing and modification access to owners

--- spec/features/showing_article_spec.rb
+++ spec/features/showing_article_spec.rb
@@ -40,5 +40,18 @@ RSpec.feature "Showing an Article" do
     expect(page).not_to have_link("Edit Article")
     expect(page).not_to have_link("Delete Article")
   end
- 
+
+  scenario "to signed in owner makes both links visible" do
+    login_as(@john)
+
+    visit "/"
+    click_link @article.title
+
+    expect(page).to have_content(@article.title)
+    expect(page).to have_content(@article.body)
+    expect(current_path).to eq(article_path(@article))
+
+    expect(page).to have_link("Edit Article")
+    expect(page).to have_link("Delete Article")
+  end
 end
