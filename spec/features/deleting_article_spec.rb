f99044ef425cf0a81f06d95f7b875b499796b9f5
Emmanuel Asante <emmanuela.asante@gmail.com>
2016-07-18 23:09:45 -0600

    Fix broken Articles Specs

--- spec/features/deleting_article_spec.rb
+++ spec/features/deleting_article_spec.rb
@@ -3,8 +3,14 @@ require "rails_helper"
 RSpec.feature "Deleting an Article" do
 
   before do
+    john = User.create(email: "john@example.com",
+                       password: "password")
+
+    login_as(john)
+
     @article = Article.create(title: "The first article",
-                body: "Lorem ipsum dolor sit amet, consectetur.")
+                body: "Lorem ipsum dolor sit amet, consectetur.",
+                user: john)
   end
 
   scenario "with correct id" do
@@ -16,5 +22,5 @@ RSpec.feature "Deleting an Article" do
     expect(page).to have_content("Article has been deleted")
     expect(current_path).to eq(articles_path)
   end
- 
+
 end
